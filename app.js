101185f1a21facadd1a34abeeaa41467c5ff84b7
Meli7 <14030642@itcelaya.edu.mx>
2018-12-09 17:42:04 -0600

    Crear modelos

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 //Este archivo será el que ejecute nuestra aplicación y arranque nuestro
 //servidor
 
-var express = require("express"),
-app = express(),
-bodyParser = require("body-parser"),
-methodOverride = require("method-override");
-mongoose = require('mongoose');
+var express  = require("express"),
+    app      = express(),
+    http     = require("http"),
+    bodyParser  = require("body-parser"),
+    methodOverride = require("method-override");
+    server   = http.createServer(app),
+    mongoose = require('mongoose');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -13,12 +15,17 @@ app.use(methodOverride());
 
 var router = express.Router();
 
-router.get('/', function(req, res){
-  res.send("Hellooooooo!");
+router.get('/', function(req, res) {
+   res.send("Hello World!");
 });
 
 app.use(router);
 
-app.listen(3000, function(){
-  console.log("Node server running on http://localhost:3000");
+mongoose.connect('mongodb://localhost/tvshows', function(err, res) {
+  if(err) {
+    console.log('ERROR: connecting to Database. ' + err);
+  }
+  app.listen(3000, function() {
+    console.log("Node server running on http://localhost:3000");
+  });
 });
